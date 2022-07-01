3a62d35692ed6ce44533b474c2298a83e41d855e
Márcio Rodrigues <marcio.mrcr@outlook.com>
2022-07-01 15:40:43 -0300

    Configurado o envio de email

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,21 +1,43 @@
 
 import express from 'express'
+import nodemailer from 'nodemailer'
 import { prisma } from './prisma'
 
 const app = express()
 
 app.use(express.json())
 
+const transport = nodemailer.createTransport({
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: "b1a28aa0ec7f8b",
+    pass: "2a3c7dc7563f8a"
+  }
+});
+
 app.post('/feedbacks', async (req, res) =>
 {
   const { type, comment, screenshot} = req.body
-  
+
   const feedback = await prisma.feedback.create({
     data: {
       type,
       comment,
       screenshot
     }
+  })
+
+   await transport.sendMail({
+    from: 'Equipe Feedget <feedback@feedget.com>',
+    to: 'Márcio Rodrigues <marcio.mrcr@outlook.com>',
+    subject: 'Novo feedback',
+    html: [
+      `<div style="font-family: sans-serif; font-size: 14px; color: #111;">`,
+      `<p>Tipo de Feedback: ${type}</p>`,
+      `<p>Comentário: ${comment}</p>`,
+      `</div>`
+    ].join('\n')
   })
 
  return res.status(201).json( { data: feedback})

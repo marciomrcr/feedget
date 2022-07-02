7248757f62688e7fcd79ab336a00ae92108cdd4e
Márcio Rodrigues <marcio.mrcr@outlook.com>
2022-07-02 19:39:26 -0300

    teste de comando de inicialização

--- server/src/server.ts
+++ server/src/server.ts
@@ -11,5 +11,5 @@ app.use(express.json())
 app.use(routes)
 
 app.listen(process.env.PORT || 3333, ()=> {
-  console.log('Server is running. Glory to God!')
+  console.log('Server is running. Glory to God!!!')
 })

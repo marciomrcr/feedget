1400cd4915cc4359e718233992fa96324e0631c3
Márcio Rodrigues <marcio.mrcr@outlook.com>
2022-07-02 19:56:04 -0300

    desbloqueio da cors

--- server/src/server.ts
+++ server/src/server.ts
@@ -4,9 +4,9 @@ import cors from 'cors'
 
 const app = express()
 
-app.use(cors({
-  origin: 'http://localhost:3000'
-}))
+app.use(cors(
+  //{  origin: 'http://localhost:3000'}
+  ))
 app.use(express.json())
 app.use(routes)
 
